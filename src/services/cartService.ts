6d4d05434678c780c900c218bbaf1e115ce4be4d
Bubbl33s <jairo.lf.8@gmail.com>
2024-10-11 14:43:45 -0500

    feat: add cart service

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -9,9 +9,67 @@ export class CartService {
   }
 
   static async getCartByUserId(userId: string) {
-    return prisma.cart.findFirst({
+    return prisma.cart.findUnique({
       where: { userId },
       include: { items: true },
+    });
+  }
+
+  static async addCartItem(
+    cartId: string,
+    productId: string,
+    quantity: number,
+  ) {
+    const cartExists = await this.getCartById(cartId);
+
+    if (!cartExists) {
+      throw new Error("No existe un carrito con ese ID");
+    }
+
+    // Check if the product is already in the cart
+    const cartItemExists = await prisma.cartItem.findFirst({
+      where: { cartId, productId },
+    });
+
+    if (cartItemExists) {
+      return this.updateCartItemQuantity(cartItemExists.id, quantity);
+    }
+
+    return prisma.cartItem.create({
+      data: {
+        cartId,
+        productId,
+        quantity,
+      },
+    });
+  }
+
+  static async updateCartItemQuantity(cartItemId: string, quantity: number) {
+    const cartItemExists = await prisma.cartItem.findUnique({
+      where: { id: cartItemId },
+    });
+
+    if (!cartItemExists) {
+      throw new Error("No existe un item de carrito con ese ID");
+    }
+
+    return prisma.cartItem.update({
+      where: { id: cartItemId },
+      data: { quantity },
+    });
+  }
+
+  static async deleteCartItem(cartItemId: string) {
+    const cartItemExists = await prisma.cartItem.findUnique({
+      where: { id: cartItemId },
+    });
+
+    if (!cartItemExists) {
+      throw new Error("No existe un item de carrito con ese ID");
+    }
+
+    return prisma.cartItem.delete({
+      where: { id: cartItemId },
     });
   }
 }
