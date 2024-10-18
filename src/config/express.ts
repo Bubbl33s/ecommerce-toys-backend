541bed288cec5f44798f16452988715c02a7c557
Bubbl33s <jairo.lf.8@gmail.com>
2024-10-18 09:37:20 -0500

    fix: origins *

--- src/config/express.ts
+++ src/config/express.ts
@@ -6,7 +6,7 @@ import morgan from "morgan";
 
 export default function setupExpress(app: Application) {
   const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type"],
   };
